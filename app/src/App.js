27297f2ef98df5bdabc9bd9333c6db5fbf295bd7
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-12-04 17:57:05 +0000

    Refactor

--- app/src/App.js
+++ app/src/App.js
@@ -1,33 +1,12 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import { checkIfWalletIsConnected } from './utils/checkIfWalletIsConnected';
 
 const BUILDSPACE_TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${BUILDSPACE_TWITTER_HANDLE}`;
 
 const App = () => {
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { solana } = window;
-
-      if (solana) {
-        if (solana.isPhantom) {
-          console.log('Phantom wallet found!');
-
-          const response = await solana.connect({ onlyIfTrusted: true });
-          console.log(
-            'Connected with Public Key:',
-            response.publicKey.toString()
-          );
-        }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
